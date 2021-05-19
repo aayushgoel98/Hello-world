f692cf4eb4ba5fba2103dfa298c21b0d84730884
Aayush Goel <44862810+aayushgoel98@users.noreply.github.com>
2021-05-19 11:28:33 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,10 @@
+
+n1 = int(input("enter number 1 "))
+n2 = int(input("enter number 2 "))
+n3 = int(input("enter number 3 "))
+n4 = int(input("enter number 4 "))
+n5 = int(input("enter number 5 "))
+avg = ((n1+n2+n3+n4+n5)/5);
+percent = avg
+print ("average of 5 number is", avg)
+print ("percentage of 5 number is ", percent , "%")
