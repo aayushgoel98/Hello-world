8d7a8392c07eb9997c872c6f982c15b04086a715
Aayush Goel <44862810+aayushgoel98@users.noreply.github.com>
2021-05-19 12:04:43 +0530

    Add files via upload

---
+++ area-of-shapes.py
@@ -0,0 +1,18 @@
+base = float(input("please enter the length of base of the traiangle "))
+height = float(input("Please enter the length of the height of the traiangle "))
+areatriangle = (0.5*base*height)
+print ("area of the traingle is ", areatriangle)
+
+side = float(input("Please enter the length of the side of the square "))
+areasquare = (side*side)
+print ("area of the square is ", areasquare)
+
+side1 = float(input("Please enter the length of the rectangle "))
+side2 = float(input("Please enter the Breath of the rectangle "))
+arearectangle = (side1*side2)
+print ("area of the rectangle is ", arearectangle)
+
+radius = float(input("Please enter the radius of the circle "))
+areacircle = (2*3.14*radius*radius)
+print ("area of the circle is ", areacircle)
+
