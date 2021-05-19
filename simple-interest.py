23a9a351722ea556db0a73181c1da52b7fe95292
Aayush Goel <44862810+aayushgoel98@users.noreply.github.com>
2021-05-19 12:19:15 +0530

    Add files via upload

---
+++ simple-interest.py
@@ -0,0 +1,6 @@
+principle = int(input("Please enter the amount for interst to be calculated "))
+time = int(input("please enter the time in months "))
+rate = int(input("please enter the rate of interest "))
+
+si = ((principle*time*rate)/100)
+print("the interest of the given amount is ",si)
