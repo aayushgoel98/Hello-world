872aaa88bff0cbaf052ca4ac3cff9a538eaaec38
Aayush Goel <44862810+aayushgoel98@users.noreply.github.com>
2021-05-20 18:16:42 +0530

    Add files via upload

---
+++ decimal-to-other.py
@@ -0,0 +1,8 @@
+#Python Program to Convert Decimal to Binary, Octal and Hexadecimal 
+# Python program to convert decimal into other number systems
+dec = int(input("Enter number: "))
+
+print("The decimal value of", dec, "is:")
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal.")
+print(hex(dec), "in hexadecimal.")
