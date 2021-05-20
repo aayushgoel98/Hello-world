7d272e633fd9c1becc7ea8b019de52b4554b6610
Aayush Goel <44862810+aayushgoel98@users.noreply.github.com>
2021-05-20 14:17:22 +0530

    Add files via upload

---
+++ ASCII-value.py
@@ -0,0 +1,5 @@
+#WAP Python Program to Find ASCII Value of Character A, 78, C (Use built-in functions)
+a = ["A",78,"C"]
+print ("ASCII value of A ",ord("A"))
+print ("character for 78 ",chr(78))
+print ("ASCII value of C ",ord("C"))
