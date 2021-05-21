d9ff861ce48964b815d7623edd42422c159bf859
Aayush Goel <44862810+aayushgoel98@users.noreply.github.com>
2021-05-21 13:43:55 +0530

    Add files via upload

---
+++ tuple-sums.py
@@ -0,0 +1,11 @@
+#WAP to sum of tuple elements
+
+tu = (3,5,7,9,11)   	#Interger values
+
+print("Original tuple " , tu)	
+li = list(tu)			#Conversion to list
+print("Original list ",li)
+
+res = sum(list(tu))		#sum of elements
+
+print ("sum of the tuple values is ",res)
